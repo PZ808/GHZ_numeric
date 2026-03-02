8f20f3f814fa9acfe581bc0492b9fc5d983c5519
PZ808 <106532737+PZ808@users.noreply.github.com>
2026-03-02 13:06:37 +0100

    simple go between for orbit data and spectral data

---
+++ include/SourceTorusFrequencies.hpp
@@ -0,0 +1,24 @@
+//
+// Created by Peter Zimmerman on 02.03.26.
+//
+
+#ifndef HS1DATA_DATA_SOURCETORUSFREQUENCIES_HPP
+#define HS1DATA_DATA_SOURCETORUSFREQUENCIES_HPP
+
+
+// SourceFrequencies.hpp
+#pragma once
+#include "GhzTypes.hpp"   // Real, Complex, Modes, etc.
+
+
+namespace orbit {
+    using Real = teuk::Real;
+
+    struct SourceFrequencies {
+        Real Omega_r{0};
+        Real Omega_z{0};
+        Real Omega_phi{0};
+    };
+
+} //orbit
+#endif //HS1DATA_DATA_SOURCETORUSFREQUENCIES_HPP
